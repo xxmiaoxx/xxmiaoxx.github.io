037f9e3e28c0134916f574d47e1609af3221a1e3
xxmiaoxx <105196049+xxmiaoxx@users.noreply.github.com>
2026-09-14 14:10:55 +0800

    Bump app cache to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='followup-v14';
+const CACHE='followup-v15';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/favicon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));});
