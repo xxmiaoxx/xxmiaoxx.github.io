b72da7d9a16aa129295f9337d4f854e2bdea50e8
xxmiaoxx <105196049+xxmiaoxx@users.noreply.github.com>
2026-09-14 11:05:05 +0800

    Refresh app cache for activation interval update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='followup-v3';
+const CACHE='followup-v4';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/favicon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))));});
